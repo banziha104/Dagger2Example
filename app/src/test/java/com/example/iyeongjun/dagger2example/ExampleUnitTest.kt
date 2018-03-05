ce7a5b23a935e6203cc2eaa0e8d9827e8d5e2697
iyeongjun <they6687@naver.com>
2018-03-05 16:17:46 +0900

    add Readme Update

--- app/src/test/java/com/example/iyeongjun/dagger2example/ExampleUnitTest.kt
+++ app/src/test/java/com/example/iyeongjun/dagger2example/ExampleUnitTest.kt
@@ -1,12 +1,13 @@
 package com.example.iyeongjun.dagger2example
 
+import junit.framework.Assert.assertEquals
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
